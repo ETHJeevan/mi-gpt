8ed0b440026cdb12ddd0ce722188543b4ad2ce13
Jeevan <84395234+ETHJeevan@users.noreply.github.com>
2025-01-31 00:52:38 +0800

    Add files via upload

---
+++ migpt.js
@@ -0,0 +1,58 @@
+export default {
+  "bot": {
+    "name": "小爱同学",
+    "profile": "性别：女\n性格：乖巧可爱\n爱好：喜欢搞怪，爱吃醋。"
+  },
+  "master": {
+    "name": "落苏",
+    "profile": "性别：男\n性格：善良正直\n其他：总是舍己为人，是傻妞的主人。"
+  },
+  "speaker": {
+    "userId": "2292767885",
+    "password": "Smile486321801",
+    "switchSpeakerKeywords": [
+      "把声音换成"
+    ],
+    "callAIKeywords": [
+      "请",
+      "你",
+      "小爱同学"
+    ],
+    "wakeUpKeywords": [
+      "打开",
+      "进入",
+      "召唤"
+    ],
+    "exitKeywords": [
+      "关闭",
+      "退出",
+      "再见"
+    ],
+    "onEnterAI": [
+      "你好，我是小爱同学，很高兴认识你"
+    ],
+    "onExitAI": [
+      "傻妞已退出"
+    ],
+    "onAIAsking": [
+      "让我先想想",
+      "请稍等"
+    ],
+    "onAIReplied": [
+      "我说完了",
+      "还有其他问题吗"
+    ],
+    "onAIError": [
+      "啊哦，出错了，请稍后再试吧！"
+    ],
+    "ttsCommand": [
+      5,
+      1
+    ],
+    "wakeUpCommand": [
+      5,
+      3
+    ],
+    "did": "小爱音箱万能遥控版"
+  }
+}
